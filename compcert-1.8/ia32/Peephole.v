56931fcdd0ab9314cc2999bb03c8f8f026920e27
Andrew Sackville-West <andrew@swclan.homelinux.org>
2011-05-05 14:30:11 -0700

    more mucking with register equality.

--- compcert-1.8/ia32/Peephole.v
+++ compcert-1.8/ia32/Peephole.v
@@ -52,15 +52,12 @@ Qed.
 
 Lemma eq_int_int__eq_z : forall x y, 
   @eq Int.int x y -> @eq Z (Int.unsigned x) (Int.unsigned y).
-Set Printing All.
 Proof.
-
   intros. f_equal. assumption.
 Qed.
 
 Lemma int_eq_false : forall x y, Int.eq x y = false -> x <> y.
 Proof.
-Set Printing All.
   intros. case_eq (Int.eq x y); intros.
   rewrite H0 in H. inversion H.
   unfold not. intro. unfold Int.eq in H0.
@@ -113,12 +110,88 @@ unfold not. intros. apply n. inversion H1. reflexivity.
 
 unfold val_eq in H. case_eq (zeq b b0).
 
-SearchAbout zeq. intros. SearchAbout zeq. rewrite H0 in H. unfold not. intro. inversion H1.
+intros. rewrite H0 in H. unfold not. intro. inversion H1.
 apply int_eq_false in H. apply H. assumption.
 
-intros. SearchAbout zeq. unfold not. intro. inversion H1. apply n. assumption.
+intros. unfold not. intro. inversion H1. apply n. assumption.
 Defined.
 
+Eval simpl in (val_eq_dec Vundef Vundef).
+Eval compute in (val_eq_dec Vundef Vundef).
+Eval compute in (val_eq_dec Vundef Vzero).
+(* appears to work *)
+
+
+Theorem val_eq_correct : forall (v1 v2 : val), val_eq v1 v2 = true -> v1 = v2.
+Proof.
+  intro v1.
+  intros.
+  case_eq (val_eq_dec v1 v2). intros. assumption.
+  intros. induction v1; induction v2; intros; try reflexivity; try discriminate. inversion H. 
+  f_equal; apply int_eq_true; assumption.
+  inversion H. destruct (Float.eq_dec f f0). f_equal; auto. inversion H2.
+  inversion H.  destruct (zeq b b0); f_equal. assumption. apply int_eq_true. assumption.
+  inversion H2. inversion H2.
+Qed.
+ 
+
+
+(* Issues raised in discussion with Xavier Leroy:
+
+   PC -- as I suspected, the PC needs to be special cased in the
+   comparison of states. Specifically we need to prove the semantic
+   equivalence of the dereferencing of the PC, not it's value. This
+   becomes particularly difficult if the PC is being stored in memory,
+   because different PC values will show up in memory and we have no
+   way to compare them at the value level. A memory entry that holds
+   the PC could have a different value with the same semantic meaning,
+   but how does one show that these are semantically equivalent.
+
+   This is really only a problem with the addition or removal of
+   instructions. Simple 1-to-1 instruction replacement is not a
+   problem as the PC is not changed (modulo branching
+   instructions). One solution, which would solve the case of
+   instruction removal, is to *not* remove the instructions, but
+   instead replace them with NOP until after the validation. This
+   would leave the PC unchanged by the transformation. Subsequently,
+   the NOPs could be removed and perhaps that transformation could be
+   validated in a different manner --> forall instr in program, if
+   instr != nop, instr in program', or some such. This does not solve
+   the problem of inserting instructions.
+
+   Another approach is to make the comparison between Mach and the
+   transformed ASM instead of between two sets of ASM -- we could
+   potentially reuse the existing proof structure to show
+   equivalence. However, this doesn't work because the current proof
+   is not a translation validation -- there is no infrastructure for
+   comparing the semantics of the two levels directly. I'm not sure
+   how tractable that problem is and whether it is feasible in the
+   timeframe available.
+
+*)
+
+Definition reg_eq (r : preg) (rs1 rs2 : regset) : {rs1 r = rs2 r} + {rs1 r <> rs2 r}.
+  refine (fun r rs1 rs2 => val_eq_dec (rs1 r) (rs2 r)).
+Defined.
+
+Definition regs_eq (rs1 rs2 : regset) : bool.
+  refine (fun rs1 rs2 => 
+  if val_eq_dec (rs1 PC) (rs2 PC) 
+    then if val_eq_dec (rs1 (IR EAX)) (rs2 (IR EAX))
+      then true
+      else false
+    else false).
+Defined.
+
+Theorem rs_eq__r_eq : forall (r : preg) (rs1 rs2 : regset), regs_eq rs1 rs2 = true -> rs1 r = rs2 r.
+Proof.
+  intro r. induction r.
+  intros. unfold regs_eq in H. destruct (val_eq_dec (rs1 PC) (rs2 PC)). assumption. inversion H.
+
+(* what does the equal sign mean here? In other words, how do we specify that two PCs are equivalent? *)
+
+Inductive pc_equiv :=
+| 
 
 (*Inductive ireg: Type :=
   | EAX: ireg  | EBX: ireg  | ECX: ireg  | EDX: ireg
@@ -153,6 +226,12 @@ Inductive preg: Type :=
   | RA: preg.                   (**r pseudo-reg representing return address *)
 *) 
   
+
+(* Here we attempt to define equality of register sets. Two registers
+sets, rs1 rs2, are equivalent if (for all R : preg, R <> PC, rs1 R =
+rs2 R) /\ rs1 PC equiv rs2 PC) for some equivalence relation on the
+dereference of PCs. *)
+
 
 Theorem test : exec1 = exec2.
 simpl. unfold exec1. unfold exec2. simpl. 
