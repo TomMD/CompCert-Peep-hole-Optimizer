78c9c95f24f5914180821781ff0fb802f73a3809
Thomas M. DuBuisson <Thomas.DuBuisson@gmail.com>
2011-05-20 11:35:14 -0700

    Update the ML code and location

--- compcert-1.8/ia32/Peephole.ml
+++ compcert-1.8/ia32/Peephole.ml
@@ -1,1 +1,10 @@
-let peephole (f: list instruction) : list instruction = f
+(* A Peephole optimizer for use in the Compcert framework *)
+open Camlcoq
+open Datatypes
+open Asm
+
+let ml_peephole (f: instruction list) : (instruction list) =
+  match f with
+  | ((Pmov_rm r1 m1) :: (Pmov_mr m2 r2) ::c::d::xs) when r1 == r2 && m1 == m2 ->
+	Pmov_rm r1 m1 :: nop :: c :: xs
+  | _ -> f
--- compcert-1.8/ia32/Peephole.mli
+++ compcert-1.8/ia32/Peephole.mli
@@ -1,1 +1,4 @@
+(* A peephole optimizer for the CompCert framework *)
+open Instructions
+
 val ml_peephole : list instruction -> list instruction
