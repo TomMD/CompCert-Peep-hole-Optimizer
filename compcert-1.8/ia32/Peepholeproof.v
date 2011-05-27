dbff851580684c7068fe751f66ff0f32914b26be
Andrew Sackville-West <andrew@swclan.homelinux.org>
2011-05-26 17:48:32 -0700

    start sketching in proof.
    mostly unusable stuff, but it's got ideas flowing for me.

--- compcert-1.8/ia32/Peepholeproof.v
+++ compcert-1.8/ia32/Peepholeproof.v
@@ -19,11 +19,70 @@ Section PRESERVATION.
 Variable prog: Asm.program.
 Variable tprog: Asm.program.
 Hypothesis TRANSF: peephole_transf_program prog = Errors.OK tprog.
+Variable match_states : Asm.state -> Asm.state -> Prop.
+Variable measure : Asm.state -> nat.
+
+Theorem peephole_correct :
+   forall (st1 : state) (t : trace) (st1' : state),
+   step (Genv.globalenv prog) st1 t st1' ->
+   forall st2 : state,
+   match_states st1 st2 ->
+   (exists st2' : state,
+      plus step (Genv.globalenv tprog) st2 t st2' /\ match_states st1' st2') \/
+   (measure st1' < measure st1)%nat /\ t = E0 /\ match_states st1' st2.
+Proof. Admitted.
+
+Lemma transf_initial_states:
+  forall st1, Asm.initial_state prog st1 ->
+  exists st2, Asm.initial_state tprog st2 /\ match_states st1 st2.
+Proof.
+Admitted.
+(*   intros. inversion H. unfold ge0 in *.
+  econstructor; split.
+  econstructor.
+  eapply Genv.init_mem_transf_partial; eauto.
+  replace (symbol_offset (Genv.globalenv tprog) (prog_main tprog) Int.zero)
+     with (Vptr fb Int.zero).
+  econstructor; eauto. constructor. apply Mem.extends_refl.
+  split. auto. unfold parent_sp; congruence.
+  intros. repeat rewrite Pregmap.gso; auto with ppcgen.
+  destruct r; simpl; congruence.
+  unfold symbol_offset. 
+  rewrite (transform_partial_program_main _ _ TRANSF). 
+  rewrite symbols_preserved. unfold ge; rewrite H1. auto. *)
+
+
+Lemma transf_final_states:
+  forall st1 st2 r, 
+  match_states st1 st2 -> Asm.final_state st1 r -> Asm.final_state st2 r.
+Proof.
+Admitted.
+(*  intros. inv H0. inv H. constructor. auto. 
+  compute in H1. 
+  generalize (preg_val _ _ _ AX AG). rewrite H1. intros LD; inv LD. auto.
+Qed. *)
+
 
 Theorem transf_program_correct :
   forall (beh: program_behavior), not_wrong beh ->
   Asm.exec_program prog beh -> Asm.exec_program tprog beh.
 Proof.
-Admitted.
+  unfold exec_program; intros.
+  (* the below is a "proof" of hte same form as Mach -> Asm, but it's
+  not the right approach i think.... but it suggests a future
+  path... *)
+  eapply simulation_star_preservation; eauto.  
+  eexact  transf_initial_states.  
+  eexact transf_final_states.  
+  eexact peephole_correct.  
+Qed.
 
 End PRESERVATION.
+(* above I've sketched in the proof, based on Mach->Asm and stubbed in
+major lemmas. After some thought, thought though, I think this may not
+be the right approach. We aren't showing that two different kinds of
+things have the same semantic, but instead that two of the same kind
+of thing hav ethe same semantic. This is different in important ways. 
+
+The statement of the final theorem, though is the correct theorem. I'm
+not sure, it requires more thought. *)
