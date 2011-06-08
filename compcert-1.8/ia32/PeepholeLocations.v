f010625def684cb3b509f656b2058e6f435c44f9
Thomas M. DuBuisson <Thomas.DuBuisson@gmail.com>
2011-06-07 20:59:02 -0700

    Weakened up the locations to allow the previously committed SymExpr / addrmode swap to work.

--- compcert-1.8/ia32/PeepholeLocations.v
+++ compcert-1.8/ia32/PeepholeLocations.v
@@ -3,14 +3,13 @@ Require Import Coqlib.
 Set Implicit Arguments.
 Section LocStore.
   Variables key val : Type.
-  Variable key_eq : forall a b : key, {a = b} + {a <> b}.
-
+  Variable beq_key : forall a b : key, bool. (* {a = b} + {a <> b}. *)
 Definition a_list := list (key * val).
 
 Record LocStore : Type := mkLocStore 
   { store : a_list;
     default : key -> val;
-    dec := key_eq
+    dec := beq_key
   }.
 
 Definition initLocStore (d : key -> val) := mkLocStore nil d.
@@ -21,7 +20,7 @@ Definition update (k : key) (v : val) (s : LocStore) : LocStore :=
 Fixpoint lookup' (k : key) (s : a_list) (d : key -> val): val :=
   match s with
     | nil => d k
-    | (k',v) :: ss => if  key_eq k' k then v else lookup' k ss d
+    | (k',v) :: ss => if  beq_key k' k then v else lookup' k ss d
   end.
 
 Definition lookup (k : key) (s : LocStore) : val :=
@@ -36,17 +35,7 @@ Fixpoint elements' (l : a_list) : list key :=
 Definition elements (s : LocStore) : list key :=
   elements' (store s).
 
-Definition key_eq_bool (k k' : key) : bool.
-refine(fun k k' =>  if (key_eq k k') then true else false).
-Defined.
-
-Lemma key_eq__eq : forall k k',
-  key_eq_bool k k' = true -> k = k'.
-intros. 
-unfold key_eq_bool in H.
-destruct (key_eq k k'). auto. inversion H.
-Qed.
-
+(*
 Lemma key_eq_bool_refl : forall k,
   key_eq_bool k k = true.
 intro. simpl. unfold key_eq_bool. simpl.
@@ -55,14 +44,13 @@ elimtype False.
 apply n. reflexivity.
 Qed.
 
+
 (* lemmas about things stored in this structure *)
 Lemma update_same : forall k v s,
   lookup k (update k v s) = v.
 intros. unfold update. unfold lookup. simpl.
-destruct (key_eq k k). reflexivity.
-elimtype False.
-apply n. reflexivity.
+case_eq (beq_key k k). reflexivity. intros. rewrite beq_key_refl in H. inversion H.
 Qed.
-
+*)
 
 End LocStore.
